25a43e4f53105bd9fbe0388af8c8c6ff0f68a552
Nathan Healea <nate.healea@gmail.com>
2026-05-03 18:47:36 -0700

    refactor(home): remove color wheel from landing page and delete /wheel route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,6 @@
-import { createClient } from '@/lib/supabase/server'
-import { ColorWheelContainer } from '@/modules/color-wheel/components/color-wheel-container'
-import { getCollectionService } from '@/modules/collection/services/collection-service.server'
-import { getHueService } from '@/modules/hues/services/hue-service.server'
-import { getPaintService } from '@/modules/paints/services/paint-service.server'
-
-export default async function Home() {
-  const supabase = await createClient()
-  const { data: { user } } = await supabase.auth.getUser()
-
-  const [paintService, hueService] = await Promise.all([getPaintService(), getHueService()])
-  const [paints, hues] = await Promise.all([
-    paintService.getColorWheelPaints(),
-    hueService.getColorWheelHues(),
-  ])
-
-  let userPaintIds: Set<string> | undefined
-  if (user) {
-    const collectionService = await getCollectionService()
-    userPaintIds = await collectionService.getUserPaintIds(user.id)
-  }
-
+export default function Home() {
   return (
-    <main className="flex min-h-0 flex-1 items-center justify-center overflow-hidden">
-      <ColorWheelContainer paints={paints} hues={hues} userPaintIds={userPaintIds} />
+    <main className="flex min-h-0 flex-1 items-center justify-center">
     </main>
   )
 }
