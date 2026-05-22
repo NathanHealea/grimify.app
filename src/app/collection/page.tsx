2b83b173bca0b4df7700c9c6a3d19b2988e08346
Nathan Healea <nate.healea@gmail.com>
2026-05-22 16:20:54 -0700

    chore(collection): Fix spacing between stats and collection seach.

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -29,7 +29,7 @@ export default async function CollectionPage() {
   ])
 
   return (
-    <Main>
+    <Main className="space-y-6">
       <PageHeader>
         <PageTitle>My Collection</PageTitle>
       </PageHeader>
